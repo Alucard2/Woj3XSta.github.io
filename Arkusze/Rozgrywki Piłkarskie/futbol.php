46d1a71dc85c41ed549d66cad1d1c91c6e0de181
Wojtek <71016290+Woj3XSta@users.noreply.github.com>
2021-04-14 17:42:47 +0200

    Add files via upload

---
+++ Arkusze/Rozgrywki Piłkarskie/futbol.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="PL-pl">
+    
+<head>
+
+    <meta charset="UTF-8"/>
+    <meta name="description" content="Portal poświęcony polskiej kadrze narodowej. Chcesz zobaczyć piłkarzy i ich pozycje? Sam przetestuj!"/>
+    <meta name="keywords" content="Polska, kadra, narodowa, piłkarze, pozycje, napastnik, pomocnik, obrońca, bramkarz, kalkulator"/>
+    <meta name="author" content="Wojtek"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <title>Rozgrywki Futbolowe</title>
+    <meta http-equiv="X-UE-Compatible" content="IE=edge,chrome=1"/> 
+    <meta http-equiv="Content-Type" content="text/html"/>
+    <link rel="stylesheet" href="styl.css" type="text/css"/>
+    
+</head>
+
+<body>
+
+</body>
+
+</html>
